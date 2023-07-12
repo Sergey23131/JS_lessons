5ec3eb702f09a8f88fe063795202b22a0ed3c99f
Sergey23131 <86601508+Sergey23131@users.noreply.github.com>
2023-07-13 02:01:21 +0300

    update

---
+++ lessons/lesson1/main.js
@@ -0,0 +1,31 @@
+/*
+let arrayInfo = ['hello', 'dicks', 10, 30, 40, 12, 'ios']
+console.log('asd')
+for (let i = 0; i <= arrayInfo.length-1; i++) {
+    console.log(arrayInfo[i])
+}
+*/
+
+
+let arrayUsers = [
+    {
+        name:'Allan',
+        username:'soila',
+        password: 'asio'
+    },
+    {
+        name:'Allan',
+        username:'soila',
+        password: 'a513o'
+    },
+    {
+        name:'Allan',
+        username:'soila',
+        password: 'aa231o'
+    }
+]
+
+for (let i = 0; i <= arrayUsers.length-1; i++) {
+    console.log(arrayUsers[i].password)
+}
+
