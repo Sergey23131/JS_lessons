2b32c047d52a4810d06b7d8a1c1a26669842e5d6
Sergey23131 <86601508+Sergey23131@users.noreply.github.com>
2023-07-17 23:38:06 +0300

    lesson 4

--- lessons/lesson1/main.js
+++ lessons/lesson1/main.js
@@ -5,7 +5,7 @@ for (let i = 0; i <= arrayInfo.length-1; i++) {
     console.log(arrayInfo[i])
 }
 */
-let products = [
+/*let products = [
     {
         title: 'milk',
         price: 22,
@@ -40,5 +40,100 @@ for (const ArrayItems of products) {
     document.write('</h3>')
 
     document.write('</div>')
+}*/
+
+/*
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+
+for (const ArrayItems of users) {
+    if (ArrayItems.age > 30) {
+        document.write(`${ArrayItems.name + ' age -' + ArrayItems.age}`)
+
+    }
+
+}*/
+
+/*
+function square(...array) {
+
+    for (let arrayElement of array) {
+        console.log(arrayElement)
+    }
+
 }
 
+square(2, 4, 'hello', 'pussie')
+*/
+
+/*function createParagraph(text) {
+    let paragraph = document.createElement("p");
+    paragraph.textContent = text;
+    document.body.appendChild(paragraph);
+}
+
+
+createParagraph("Hello, world!");
+
+
+let myText = "This is my paragraph.";
+createParagraph(myText);*/
+
+
+/*
+function createUl(text, number) {
+    let list = document.createElement('ul')
+    for (let i = 0; i < number; i++) {
+        let stroke = document.createElement('li');
+        stroke.textContent = text;
+        list.appendChild(stroke)
+    }
+    document.body.appendChild(list)
+}
+
+
+createUl('Logic', 10)
+*/
+
+/*function createUl(...arrNumbers) {
+    let sum = 0;
+    for (const ArrNumber of arrNumbers) {
+
+        sum += ArrNumber;
+
+    }
+    return sum
+}
+
+
+console.log(createUl(30, 10, 10))*/
+
+/*створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
+Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]*/
+
+
+
+function swap(arr, index1, index2) {
+
+    // Меняем значения местами с использованием временной переменной
+    let temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+
+    return arr;
+}
+
+// Пример использования
+let array = [11, 22, 33, 44];
+console.log(swap(array, 0, 1)); // [22, 11, 33, 44]
