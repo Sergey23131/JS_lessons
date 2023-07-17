706c607cf401a336019086d2ba83920a0e3acb48
Sergey23131 <86601508+Sergey23131@users.noreply.github.com>
2023-07-18 01:08:57 +0300

    lesson 6

--- lessons/lesson1/main.js
+++ lessons/lesson1/main.js
@@ -123,8 +123,7 @@ console.log(createUl(30, 10, 10))*/
 Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]*/
 
 
-
-function swap(arr, index1, index2) {
+/*function swap(arr, index1, index2) {
 
     // Меняем значения местами с использованием временной переменной
     let temp = arr[index1];
@@ -136,4 +135,155 @@ function swap(arr, index1, index2) {
 
 // Пример использования
 let array = [11, 22, 33, 44];
-console.log(swap(array, 0, 1)); // [22, 11, 33, 44]
+console.log(swap(array, 0, 1)); // [22, 11, 33, 44]*/
+
+/*
+Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250*/
+
+
+/*
+function exchange(sumUAH, currencyValues, exchangeCurrency) {
+    let result;
+    for (const currencyValuesItter of currencyValues) {
+        if (currencyValuesItter.currency === exchangeCurrency) {
+            result = sumUAH / currencyValuesItter.value;
+            return result
+        }
+    }
+
+}
+
+
+console.log(exchange(40, [{currency: 'USD', value: 40}, {currency: 'EUR', value: 42}], 'EUR'))
+*/
+
+/*
+Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+    let str = 'Ревуть воли як ясла повні';
+let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']*/
+
+/*let str = 'Ревуть воли як ясла повні'
+
+function stringToarray(str) {
+    let arr = str.split(' ')
+    console.log(arr)
+}
+
+stringToarray(str)*/
+
+/*
+- є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
+*/
+/*let numbers = [10, 8, -7, 55, 987, -1011, 0, 1050, 0];
+
+let doubledNumbers = numbers.map(function (num) {
+    return num.toString();
+});
+console.log(doubledNumbers)*/
+/*
+
+- створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
+    let nums = [11,21,3];
+sortNums(nums,'ascending') // [3,11,21]
+sortNums(nums,'descending') // [21,11,3]*/
+
+
+/*let numbers = [5, 2, 8, 1, 4];
+
+function sortNums(number, action) {
+    if (action === 'ascending') {
+        console.log(numbers.sort(function (a, b) {
+            return b - a;
+        }))
+    }
+    if (action === 'descending') {
+        console.log(numbers.sort(function (a, b) {
+            return a - b;
+        }))
+    }
+
+}
+
+sortNums(numbers, 'ascending'); // [8, 5, 4, 2, 1]
+
+sortNums(numbers, 'descending'); // [8, 5, 4, 2, 1]*/
+/*
+
+- є масив
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+-- відсортувати його за спаданням за monthDuration
+-- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+-- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}*/
+/*
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 3},
+    {title: 'Frontend', monthDuration: 4}
+];
+*/
+
+
+/*
+-- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+
+function filterByDuration(arr) {
+    let filteredArray = arr.filter(function (item) {
+        return item.monthDuration > 5;
+    });
+
+    return filteredArray;
+}
+
+coursesAndDurationArray = filterByDuration(coursesAndDurationArray);
+console.log(coursesAndDurationArray);*/
+
+/*
+-- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+*/
+
+
+/*let transformedArray = coursesAndDurationArray.map(function (item, index) {
+    return {
+        id: index + 1,
+        title: item.title,
+        monthDuration: item.monthDuration
+    };
+});
+
+console.log(transformedArray);*/
+
+
+/*описати колоду карт (від 6 до туза без джокерів)
+- знайти піковий туз
+- всі шістки
+- всі червоні карти
+- всі буби
+- всі трефи від 9 та більше
+
+{
+    cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
+        value: '', // '6'-'10', 'ace','jack','queen','king','joker'
+    color:'', // 'red','black'
+}
+
+=========================
+
+    Взяти описану колоду карт, та за допомоги reduce упакувати всі карти по "мастях" в об'єкт
+{
+    spades:[],
+        diamonds:[],
+    hearts:[],
+    clubs:[]
+}
+=========================*/
