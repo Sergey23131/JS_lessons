802f0bf73d000601433e3a1f749c54681cdfc2fb
Sergey23131 <86601508+Sergey23131@users.noreply.github.com>
2023-07-18 23:25:50 +0300

    lesson 6

--- lessons/lesson1/main.js
+++ lessons/lesson1/main.js
@@ -287,3 +287,66 @@ console.log(transformedArray);*/
     clubs:[]
 }
 =========================*/
+
+let deck = [
+    {cardSuit: 'spade', value: '6', color: 'black'},
+    {cardSuit: 'spade', value: '7', color: 'black'},
+    {cardSuit: 'spade', value: '8', color: 'black'},
+    {cardSuit: 'spade', value: '9', color: 'black'},
+    {cardSuit: 'spade', value: '10', color: 'black'},
+    {cardSuit: 'spade', value: 'ace', color: 'black'},
+    {cardSuit: 'spade', value: 'jack', color: 'black'},
+    {cardSuit: 'spade', value: 'queen', color: 'black'},
+    {cardSuit: 'spade', value: 'king', color: 'black'},
+    {cardSuit: 'diamond', value: '6', color: 'red'},
+    {cardSuit: 'diamond', value: '7', color: 'red'},
+    {cardSuit: 'diamond', value: '8', color: 'red'},
+    {cardSuit: 'diamond', value: '9', color: 'red'},
+    {cardSuit: 'diamond', value: '10', color: 'red'},
+    {cardSuit: 'diamond', value: 'ace', color: 'red'},
+    {cardSuit: 'diamond', value: 'jack', color: 'red'},
+    {cardSuit: 'diamond', value: 'queen', color: 'red'},
+    {cardSuit: 'diamond', value: 'king', color: 'red'},
+    {cardSuit: 'heart', value: '6', color: 'red'},
+    {cardSuit: 'heart', value: '7', color: 'red'},
+    {cardSuit: 'heart', value: '8', color: 'red'},
+    {cardSuit: 'heart', value: '9', color: 'red'},
+    {cardSuit: 'heart', value: '10', color: 'red'},
+    {cardSuit: 'heart', value: 'ace', color: 'red'},
+    {cardSuit: 'heart', value: 'jack', color: 'red'},
+    {cardSuit: 'heart', value: 'queen', color: 'red'},
+    {cardSuit: 'heart', value: 'king', color: 'red'},
+    {cardSuit: 'clubs', value: '6', color: 'black'},
+    {cardSuit: 'clubs', value: '7', color: 'black'},
+    {cardSuit: 'clubs', value: '8', color: 'black'},
+    {cardSuit: 'clubs', value: '9', color: 'black'},
+    {cardSuit: 'clubs', value: '10', color: 'black'},
+    {cardSuit: 'clubs', value: 'ace', color: 'black'},
+    {cardSuit: 'clubs', value: 'jack', color: 'black'},
+    {cardSuit: 'clubs', value: 'queen', color: 'black'},
+    {cardSuit: 'clubs', value: 'king', color: 'black'}
+];
+
+/*function aceOnefind(arr) {
+    for (let arrElement of arr) {
+        if (arrElement.cardSuit === 'spade' && arrElement.value === 'ace') {
+            return arrElement
+        }
+    }
+}*/
+/*let arrCards = []
+
+function aceOnefind(arr) {
+    for (let arrElement of arr) {
+        if (arrElement.value !== '6' && arrElement.value !== '7' && arrElement.value !== '8' && arrElement.cardSuit === 'clubs') {
+            arrCards.push(arrElement)
+        }
+    }
+    return arrCards
+}*/
+
+let result = deck.filter(function(arr) {
+    return arr.cardSuit === 'clubs' && !['6', '7', '8'].includes(arr.value);
+});
+
+console.log(result)
